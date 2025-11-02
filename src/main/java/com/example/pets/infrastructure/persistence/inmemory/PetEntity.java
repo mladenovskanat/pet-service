6730de5991380e86cfdff1a93b851cabcf4321e7
mladenovskanat <mladenovska.nat@gmail.com>
2025-11-02 22:26:39 +0100

    remove unnecessary annotations in PetEntity

--- src/main/java/com/example/pets/infrastructure/persistence/inmemory/PetEntity.java
+++ src/main/java/com/example/pets/infrastructure/persistence/inmemory/PetEntity.java
@@ -1,12 +1,7 @@
 package com.example.pets.infrastructure.persistence.inmemory;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.*;
 
-@Entity
 @Setter
 @Getter
 @NoArgsConstructor
@@ -14,9 +9,7 @@ import lombok.*;
 @Builder
 public class PetEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id; //ID managed manually since this app uses in-memory database
     private String name;
     private String species;
     private Integer age;
