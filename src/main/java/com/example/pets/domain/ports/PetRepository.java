5a8e717941954c9ef3f4f67ad1db619672fb24f1
mladenovskanat <mladenovska.nat@gmail.com>
2025-11-02 21:48:08 +0100

    remove unnecessary comment

--- src/main/java/com/example/pets/domain/ports/PetRepository.java
+++ src/main/java/com/example/pets/domain/ports/PetRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PetRepository {
-    Pet save(Pet pet);                   // create or update
+    Pet save(Pet pet);
 
     Optional<Pet> findById(Long id);
 
